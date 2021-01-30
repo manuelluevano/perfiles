1267a236285bcc746decf96dfa84a88c6ff17bd7
Manuel Luevano 'macbook pro <manuelluevano@MacBook-Pro-de-Manuel.local>
2021-01-29 22:49:05 -0800

    Se agrego error.sass / se agrego validador de input 'nombre'

--- js/main.js
+++ js/main.js
@@ -5,5 +5,32 @@
 
     console.log("Página cargada!")
 
+
+    /*   Variables de los inputs  */ 
+     var nombre = document.getElementById('nombre');
+     var email = document.getElementById('email');
+     var pass = document.getElementById('pass');
+     var fecha = document.getElementById('fecha');
+     var link_perfil = document.getElementById('link-perfil');
+     var numero = document.getElementById('numero');
+     var link_perfil_real = document.getElementById('link-perfil-real');
+     var pais = document.getElementById('pais');
+
+
+     var error = document.getElementById('error');
+
+    /*   Valida inputs */
+    
+    
+    nombre.addEventListener('blur', function(){
+        
+        if(this.value == ''){
+            error.innerHTML = 'Completa todos los campos';
+            this.style.border = "1px solid red";
+            error.style.borderBottom = "2px solid red";
+        }
+    });
+
+
     });  //DOM Content Loaded
 })();
