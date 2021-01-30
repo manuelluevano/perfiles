0feb206c8f142d33d678fdb19e03ac6746a4f38f
Manuel Luevano 'macbook pro <manuelluevano@MacBook-Pro-de-Manuel.local>
2021-01-30 15:51:07 -0800

    Agregado: verificación para inpust generales e Email

--- js/main.js
+++ js/main.js
@@ -12,25 +12,49 @@
      var pass = document.getElementById('pass');
      var fecha = document.getElementById('fecha');
      var link_perfil = document.getElementById('link-perfil');
-     var numero = document.getElementById('numero');
+     var numero = document.getElementById('telefono');
      var link_perfil_real = document.getElementById('link-perfil-real');
      var pais = document.getElementById('pais');
 
 
      var error = document.getElementById('error');
 
+
     /*   Valida inputs */
     
-    
-    nombre.addEventListener('blur', function(){
-        
+    nombre.addEventListener('blur', validarCampos);
+    email.addEventListener('blur', validarCampoEmail);
+    pass.addEventListener('blur', validarCampos);
+    link_perfil.addEventListener('blur', validarCampos);
+    numero.addEventListener('blur', validarCampos);
+    link_perfil_real.addEventListener('blur', validarCampos);
+
+    /*  Function validar inputs */
+    function validarCampos(){
         if(this.value == ''){
             error.innerHTML = 'Completa todos los campos';
             this.style.border = "1px solid red";
             error.style.borderBottom = "2px solid red";
         }
-    });
+        else{
+            error.innerHTML = "";
+            this.style.border = "1px solid #e1e1e1";
+        }
+    }
 
+    /** Validar campo email  **/
+    function validarCampoEmail(){
+        if(email.value.indexOf("@") > -1){
+            error.style.display = 'none';
+            error.style.borderBottom = 'none';
+        }else{
+            error.style.display = 'block';
+            error.innerHTML = 'Debe contener un @';
+            error.style.borderBottom = '2px solid red';
+
+
+        }
+    }
 
     });  //DOM Content Loaded
 })();
