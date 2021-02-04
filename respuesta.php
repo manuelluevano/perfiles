91be9cc7831a15c616c202c0cced142c081ee61b
Manuel Luevano 'macbook pro <manuelluevano@MacBook-Pro-de-Manuel.local>
2021-02-03 21:50:41 -0800

    Agregando seguridad a la validación de inputs -> FILTER_SANITIZE_STRING

--- respuesta.php
+++ respuesta.php
@@ -57,15 +57,17 @@
       if(! (filter_has_var(INPUT_POST, 'nombre')  && (strlen(filter_input(INPUT_POST, 'nombre')) > 0 ))){
         echo "El nombre es obligatorio";
       }else{ ?>
-        <p> Nombre: <?php echo $nombre; ?> </p>
+        <?php $nuevo_nombre = filter_var($nombre, FILTER_SANITIZE_STRING); ?>
+        <p> Nombre: <?php echo $nuevo_nombre; ?> </p>
       
 <?php }    ?>
 <hr>
 <?php 
       if(! (filter_has_var(INPUT_POST, 'email')  && (strlen(filter_input(INPUT_POST, 'email')) > 0 ))){
-        echo "El correo es obligatorio";
+        echo "El email es obligatorio";
       }else{ ?>
-        <p> Correo electronico: <?php echo $email; ?> </p>
+        <?php $nuevo_email = filter_var($email, FILTER_SANITIZE_STRING); ?>
+        <p> Correo electronico: <?php echo $nuevo_email; ?> </p>
       
 <?php }    ?>
 <hr>
@@ -73,7 +75,8 @@
       if(! (filter_has_var(INPUT_POST, 'link')  && (strlen(filter_input(INPUT_POST, 'link')) > 0 ))){
         echo "El link es obligatorio";
       }else{ ?>
-        <p> Link (nuevo perfil): <?php echo $link; ?> </p>
+        <?php $nuevo_link = filter_var($link, FILTER_SANITIZE_STRING); ?>
+        <p> Link: <?php echo $nuevo_link; ?> </p>
       
 <?php }    ?>
 <hr>
@@ -81,10 +84,10 @@
       if(! (filter_has_var(INPUT_POST, 'numero')  && (strlen(filter_input(INPUT_POST, 'numero')) > 0 ))){
         echo "El numero es obligatorio";
       }else{ ?>
-        <p> Número: <?php echo $numero; ?> </p>
+        <?php $nuevo_numero = filter_var($numero, FILTER_SANITIZE_STRING); ?>
+        <p> Número: <?php echo $nuevo_numero; ?> </p>
       
 <?php }    ?>
-
 <hr>
 
 <?php if(($fecha) > 0) { ?>
